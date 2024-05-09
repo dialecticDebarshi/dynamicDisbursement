683be7845e16b182d2ad21cf7ae69ecbe5680844
dialecticDebarshi <140203504+dialecticDebarshi@users.noreply.github.com>
2024-05-09 17:48:41 +0530

    Add files via upload

---
+++ DbAccess.cs
@@ -0,0 +1,154 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+using System.Net.Mail;
+
+namespace API_Disbursement.Utility
+{
+    public class DbAccess
+    {
+        private readonly string _connectionString;
+        public static byte[] pImage;
+
+        public DbAccess(IConfiguration configuration)
+        {
+            // Read the connection string from the configuration file
+            _connectionString = configuration.GetConnectionString("connection");
+        }
+
+        public DataTable GetDataTable(SqlCommand query)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                query.Connection = connection;
+                query.CommandType = CommandType.StoredProcedure;
+                using (query)
+                {
+                    connection.Open();
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(query))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                    connection.Close();
+                }
+            }
+
+            return dataTable;
+        }
+		public int ExecuteNonQuery(string query, string[] parametername, string[] parametervalue)
+		{
+			//int rowsAffected = 0;
+			SqlConnection con = new SqlConnection(_connectionString);
+			SqlCommand cmd = new SqlCommand(query, con);
+			cmd.CommandTimeout = 0;
+			cmd.CommandType = CommandType.StoredProcedure;
+			for (int i = 0; i < parametername.Length; i++)
+			{
+				if (parametername[i] == "@photo")
+				{
+					cmd.Parameters.AddWithValue(parametername[i], pImage);
+				}
+				else
+				{
+					cmd.Parameters.AddWithValue(parametername[i], parametervalue[i]);
+				}
+			}
+			con.Open();
+			SqlParameter returnParameter = cmd.Parameters.Add("RetVal", SqlDbType.Int);
+			returnParameter.Direction = ParameterDirection.ReturnValue;
+
+			cmd.ExecuteNonQuery();
+			int id = (int)returnParameter.Value;
+			con.Dispose();
+			return id;
+
+
+			
+		}
+        public int ExecuteNonQuery(SqlCommand cmd)
+        {
+            //int rowsAffected = 0;
+            SqlConnection con = new SqlConnection(_connectionString);
+            cmd.Connection = con;
+           
+            cmd.CommandTimeout = 0;
+            cmd.CommandType = CommandType.StoredProcedure;
+            
+            con.Open();
+            SqlParameter returnParameter = cmd.Parameters.Add("RetVal", SqlDbType.Int);
+            returnParameter.Direction = ParameterDirection.ReturnValue;
+            
+            cmd.ExecuteNonQuery();
+            int id = (int)returnParameter.Value;
+            con.Dispose();
+            return id;
+
+
+            //using (SqlConnection connection = new SqlConnection(_connectionString))
+            //{
+
+            //    query.Connection = connection;
+            //    query.CommandType = CommandType.StoredProcedure;
+            //    using (query)
+            //    {
+            //        connection.Open();
+            //        rowsAffected = query.ExecuteNonQuery();
+            //        connection.Close();
+            //    }
+            //}
+
+            //return rowsAffected;
+        }
+        public DataSet Ds_Process(String Storp, string[] parametername, string[] parametervalue)
+        {
+
+            try
+            {
+
+                SqlConnection con = new SqlConnection(_connectionString);
+                SqlCommand cmd = new SqlCommand(Storp, con);
+                cmd.CommandTimeout = 0;
+                cmd.CommandType = CommandType.StoredProcedure;
+                for (int i = 0; i < parametername.Length; i++)
+                {
+                    cmd.Parameters.AddWithValue(parametername[i], parametervalue[i]);
+                }
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                da.Dispose();
+                con.Dispose();
+                return ds;
+            }
+            catch (Exception ex)
+            {
+
+                DataSet ds = null;
+                return ds;
+            }
+
+        }
+        public void SendEmail(string recipientEmail, string subject, string body)
+        {
+            string senderEmail = "technoconsalary@gmail.com"; // Replace with the sender email address
+            string senderPassword = "technoconsalary123"; // Replace with the sender email password
+
+            using (MailMessage mailMessage = new MailMessage(senderEmail, recipientEmail))
+            {
+                mailMessage.Subject = subject;
+                mailMessage.Body = body;
+                mailMessage.IsBodyHtml = true;
+
+                using (SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 465)) // Replace with the SMTP server details
+                {
+                    smtpClient.Credentials = new NetworkCredential(senderEmail, senderPassword);
+                    smtpClient.EnableSsl = true;
+                    smtpClient.Send(mailMessage);
+                }
+            }
+        }
+    }
+}
---
+++ Disbursement.cs
@@ -0,0 +1,121 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Xml.Linq;
+
+namespace API_Disbursement.Models
+{
+    public class Disbursement
+    {
+        //public IFormFile? FileUpload { set; get; }
+
+        //public string? FilePath { get; set; }
+
+        public int? TenantID { get; set; }
+		// public string? LocationName { get; set; }
+
+		public int? CompanyKey { get; set; }
+		public int? HEAD_DISBURSEMENT_KEY { get; set; }
+        public int? HEAD_KEY { get; set; }
+
+
+
+        public string? DISBURSEMENT_HEADING { get; set; }
+
+        public string? DISBURSEMENT_REFNO { get; set; }
+        public string? DISBURSEMENT_REF_DATE { get; set; }
+        public string? DISBURSEMENT_REMARKS { get; set; }
+		
+		public int? MAST_DISBURSEMENT_TYPE_KEY { get; set; }
+
+		public int? COMPLETE_FLAG { get; set; }
+
+		public int? APPROVAL_TAG { get; set; }
+
+	}
+	public class DisbursementDetails : Disbursement
+	{
+		public string? ENT_DATE { get; set; }
+
+		//EDIT_DATE
+
+		public int? ENT_USER_KEY { get; set; }
+		//EDIT_USER_KEY
+		public int? EMPLOYEE_MASTER_KEY { get; set; }
+		public int? TAG_DELETE { get; set; }
+		public int? DTLS_DISBURSEMENT_KEY { get; set; }
+		public int? DTLS_KEY { get; set; }
+		public int? PAY_TYPE_KEY { get; set; }
+		
+
+		public string? DISBURSEMENT_TYPE { get; set; }
+		public decimal? NET_PAYABLE_AMOUNT { get; set; }
+		public decimal? DISBURSING_AMOUNT { get; set; }
+
+		public DateTime? FR_DT { get; set; }
+
+		public DateTime? TO_DT { get; set; }
+		public string? combinedString { get; set; }
+
+		public string? SALARY_REFNO { get; set; }
+
+		public List<DataDictionary>? disburseList { get; set; }
+
+
+		public string? selectedValuescostcenter { get; set; }
+		public string? selectedValuesdivision { get; set; }
+		public string? selectedValueswarehouse { get; set; }
+
+		public string? selectedValuesEmp { get; set; }
+		public string? selectedValuesDisburse { get; set; }
+
+	}
+    public class DataDictionary
+    {
+        public string? Key { get; set; }
+        public string? Value { get; set; }
+        public string? NetAmnt { get; set; }
+    }
+    //   public partial class DisbursementDetails : Disbursement
+    //   {
+    //       public string? ENT_DATE { get; set; }
+
+    //       //EDIT_DATE
+
+    //       public int? ENT_USER_KEY { get; set; }
+    //       //EDIT_USER_KEY
+    //       public int? EMPLOYEE_MASTER_KEY { get; set; }
+    //       public int? TAG_DELETE { get; set; }
+    //       public int? DTLS_DISBURSEMENT_KEY { get; set; }
+    //       public int? DTLS_KEY { get; set; }
+    //       public int? PAY_TYPE_KEY { get; set; }
+    //       public int? MAST_DISBURSEMENT_TYPE_KEY { get; set; }
+    //       public decimal? NET_PAYABLE_AMOUNT { get; set; }
+
+    //       public decimal? DISBURSING_AMOUNT { get; set; }
+
+    //       public DateTime? FR_DT { get; set; }
+
+    //       public DateTime? TO_DT { get; set; }
+
+    //       public string? combinedString { get; set; }
+
+    //	public int? HEAD_KEY { get; set; }
+
+    //	public string? SALARY_REFNO { get; set; }
+
+    //       public List<DataDictionary> disburseList { get; set; }
+
+
+    //	public string? selectedValuescostcenter { get; set; }
+    //	public string? selectedValuesdivision { get; set; }
+    //	public string? selectedValueswarehouse { get; set; }
+
+
+    //}
+    //public class DataDictionary
+    //{
+    //	public string? Key { get; set; }
+    //	public string? Value { get; set; }
+    //}
+
+}
